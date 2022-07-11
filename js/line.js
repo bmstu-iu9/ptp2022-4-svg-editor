2ce6e5c883d8522e08df79c68b20d43559d4d107
Ilya Afanasyev <ilya.afanasyev.26@gmail.com>
2022-07-11 17:11:17 +0300

    Добавлены классы Figure и PivotPoint

--- js/line.js
+++ js/line.js
@@ -1,40 +1,72 @@
 lineWidthRange.onmousedown = function() {
-  lineWidthRange.onmousemove = function() {
-    lineWidthValue = lineWidthRange.value;
-    lineWidthIllustration.setAttribute('r', lineWidthValue);
-  };
+    lineWidthRange.onmousemove = function() {
+        lineWidthValue = lineWidthRange.value;
+        lineWidthIllustration.setAttribute('r', lineWidthValue);
+    };
 };
 
-svgPanel.addEventListener('mousedown', (event) => {
-  if (selectedTool != lineTool) {
-    return;
-  }
 
-  let line = document.createElementNS(svgNS, 'line');
+class Line extends Figure {
+    constructor(svg) {
+        super(svg);
+    }
 
-  line.setAttribute('x1', event.offsetX);
-  line.setAttribute('y1', event.offsetY);
-  setEndOfLine(event);
+    static draw(event) {
+        if (selectedTool != lineTool) {
+            return;
+        }
 
-  line.setAttribute('stroke-width', lineWidthValue);
-  line.setAttribute('stroke', selectedColor);
-  line.setAttribute('opacity', 0.5);
+        const createSvgLine = () => {
+            let svgLine = document.createElementNS(svgNS, 'line');
 
-  svgPanel.append(line);
+            svgLine.setAttribute('x1', event.offsetX);
+            svgLine.setAttribute('y1', event.offsetY);
+            svgLine.setAttribute('x2', event.offsetX);
+            svgLine.setAttribute('y2', event.offsetY);
 
-  svgPanel.addEventListener('mousemove', setEndOfLine);
-  svgPanel.addEventListener('mouseup', deleteHandlers);
-  svgPanel.addEventListener('mouseleave', deleteHandlers);
+            svgLine.setAttribute('stroke-width', lineWidthValue);
+            svgLine.setAttribute('stroke', selectedColor);
+            svgLine.setAttribute('opacity', 0.5);
 
-  function setEndOfLine(event) {
-    line.setAttribute('x2', event.offsetX);
-    line.setAttribute('y2', event.offsetY);    
-  }
+            return svgLine;
+        };
 
-  function deleteHandlers(event) {
-    svgPanel.removeEventListener('mousemove', setEndOfLine);
-    svgPanel.removeEventListener('mouseup', deleteHandlers);
+        const line = new Line( createSvgLine() );
+        svgPanel.append(line.svg);
 
-    line.setAttribute('opacity', 1);
-  }
-});
+        const onMouseMove = (event) => {
+            line.svg.setAttribute('x2', event.offsetX);
+            line.svg.setAttribute('y2', event.offsetY);
+        };
+
+        const onMouseUp = (event) => {
+            svgPanel.removeEventListener('mousemove', onMouseMove);
+            svgPanel.removeEventListener('mouseup', onMouseUp);
+            svgPanel.removeEventListener('mouseleave', onMouseUp);
+
+            line.addPivotPoint(line.x1, line.y1);
+            line.addPivotPoint(line.x2, line.y2);
+    
+            line.addCenterPivotPoint();
+
+            line.svg.setAttribute('opacity', 1);
+            line.adjustHighlighting();
+        };
+
+        svgPanel.addEventListener('mousemove', onMouseMove);
+        svgPanel.addEventListener('mouseup', onMouseUp);
+        svgPanel.addEventListener('mouseleave', onMouseUp);
+    }
+
+    set x1(value) { this.svg.setAttribute('x1', +value); }
+    set y1(value) { this.svg.setAttribute('y1', +value); }
+    set x2(value) { this.svg.setAttribute('x2', +value); }
+    set y2(value) { this.svg.setAttribute('y2', +value); }
+
+    get x1() { return +this.svg.getAttribute('x1'); }
+    get y1() { return +this.svg.getAttribute('y1'); }
+    get x2() { return +this.svg.getAttribute('x2'); }
+    get y2() { return +this.svg.getAttribute('y2'); }
+}
+
+svgPanel.addEventListener('mousedown', Line.draw);
